802f829462dcc935b05bcf53631702f589be88c9
Yiannis Stavrou <stav0049@gmail.com>
2019-12-26 10:07:15 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // JS Calculator written in OOP class inheritance pattern.
 class Config {
   constructor(name, lang) {
-    // basic configuration properties for our app.
+    // public configuration properties for our app.
     this.name = name;
     this.lang = lang;
     this.result = 0;
